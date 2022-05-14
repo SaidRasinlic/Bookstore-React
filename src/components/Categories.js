c24ae2002a35328e8945578cf1704841fd27d670
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-05-14 21:27:12 +0200

    Add categories React Component and React-Redux Hooks

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { checkStatus } from '../redux/categories/categories';
+import './Categories.css';
 
 const Categories = () => {
   const dispatch = useDispatch();
@@ -11,10 +12,10 @@ const Categories = () => {
   };
 
   return (
-    <>
-      <button type="button" onClick={handleStatus}>Check Status</button>
-      <p>{status.showStatus ? 'Under Construction' : ''}</p>
-    </>
+    <div className="category-wrapper">
+      <button className="categories-button" type="button" onClick={handleStatus}>Check Status</button>
+      {status.showStatus ? <p className="categories-para">Under Construction</p> : ''}
+    </div>
   );
 };
 export default Categories;
