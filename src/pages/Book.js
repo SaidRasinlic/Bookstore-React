06a0f59a436128a3efd540d84e89635131e11558
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-05-14 21:30:06 +0200

    Setup Book container with React Components

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -3,11 +3,17 @@ import BookList from '../components/BookList';
 import AddBook from '../components/AddBook';
 
 export default function Books() {
+  const hrStyle = {
+    marginTop: '2rem',
+    marginBottom: '1.8rem',
+    border: 'solid 1px var(--grey-border)',
+    borderBottom: 'none',
+  };
   return (
-    <>
-      <h2>Books Page</h2>
+    <div className="page-body">
       <BookList />
+      <hr style={hrStyle} />
       <AddBook />
-    </>
+    </div>
   );
 }
