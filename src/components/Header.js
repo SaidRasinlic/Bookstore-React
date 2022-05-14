76566b93e80a970530c955b077a965ace3f1e95c
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-05-14 21:27:58 +0200

    Setup React Components and pages with React Router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Header.css';
+import image from '../assets/images/user.png';
 
 export default function Header() {
   return (
-    <header>
+    <header id="header">
       <nav className="nav">
         <ul className="nav-items">
+          <li><h1 id="title">Bookstore CMS</h1></li>
           <li><NavLink to="/" className={(navData) => (navData.isActive ? 'active' : '')}>Books</NavLink></li>
           <li><NavLink to="/categories" className={(navData) => (navData.isActive ? 'active' : '')}>Categories</NavLink></li>
         </ul>
+        <img id="user-logo" src={image} alt="User" />
       </nav>
     </header>
   );
